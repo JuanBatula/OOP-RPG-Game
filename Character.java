d31746f1e30c80fcc9cfb758546d89461bba1ae9
Angela Vem Limos <angelavem.limos@cit.edu>
2026-03-05 23:51:09 +0800

    Player class

--- Character.java
+++ Character.java
@@ -23,6 +23,33 @@ abstract class Character{
     public abstract int attack();
 }
 
+class Player extends Character{
+  private int level;
+  private int experience;
+
+  public Player(String name){
+    super(name, 100, 20);
+    this.level = 1;
+    this.experience = 0;
+  }
+
+  @Override
+  public int attack(){
+    return attackPower;
+  }
+
+  public void gainExperience(int exp){
+    experience += exp;
+    if(experience >= 100){
+      level++;
+      experience = 0;
+      attackPower += 5;
+      System.out.println("Level Up! You are now level " +level);
+    }
+  }
+}
+
+
 // ouano -> Items (Inheritance) 
 class Item {
     protected String name;

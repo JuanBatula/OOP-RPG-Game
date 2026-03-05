6107bf7ca54c8fed5fc69e08d3b2015b039c1062
Angela Vem Limos <angelavem.limos@cit.edu>
2026-03-06 00:10:26 +0800

    Enemy class

--- Character.java
+++ Character.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 abstract class Character{
     protected String name;
     protected int health;
@@ -12,10 +10,11 @@ abstract class Character{
     }
 
     public void takeDamage(int damage){
-        health -= damage;
+        health -= damage;                   
         if (health < 0) health = 0;
     }
 
+    //getter 
     public int getHealth(){
         return health;
     }
@@ -28,9 +27,9 @@ class Player extends Character{
   private int experience;
 
   public Player(String name){
-    super(name, 100, 20);
-    this.level = 1;
-    this.experience = 0;
+    super(name, 100, 20);        // default health and attack power
+    this.level = 1;              // starting level
+    this.experience = 0;         // starting experience
   }
 
   @Override
@@ -38,14 +37,28 @@ class Player extends Character{
     return attackPower;
   }
 
-  public void gainExperience(int exp){
+  public void gainExperience(int exp){   // to level up
     experience += exp;
     if(experience >= 100){
       level++;
       experience = 0;
       attackPower += 5;
-      System.out.println("Level Up! You are now level " +level);
+      System.out.println("Congratz! You are now level " +level);
     }
+  }
+}
+
+class Enemy extends Character{
+  private int difficultyLevel;
+
+  public Enemy(String name, int difficultyLevel){
+    super(name, 50+difficultyLevel*10, 10+difficultyLevel*5);    // health and attack power increase with difficulty level
+    this.difficultyLevel = difficultyLevel;
+  }
+
+  @Override
+  public int attack(){
+    return attackPower;
   }
 }
 

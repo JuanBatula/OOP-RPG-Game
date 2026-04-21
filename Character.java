28ef68f8ae479ecaa7c5efe4453d394980fd3f04
Vaughn Ouano <99976252+vaughnouano@users.noreply.github.com>
2026-04-21 03:48:45 +0000

    feat: added default function to attack() method

--- Character.java
+++ Character.java
@@ -7,32 +7,29 @@ public abstract class Character {
 
         public Character(String name, int health, int maxHealth, int attackPower){
                     this.name = name;
-                            this.health = health;
-                                    this.maxHealth = maxHealth;
-                                            this.attackPower = attackPower;
+                    this.health = health;
+                    this.maxHealth = maxHealth;
+                    this.attackPower = attackPower;
         }
 
         public String getName() {
                     return name;
         }
 
-            public void takeDamage(int damage) {
-                        this.health -= damage;
-                                if (this.health < 0) {
-                                                this.health = 0;
-                                }
-                                        System.out.println(name + " takes " + damage + " damage! (HP: " + health + "/" + maxHealth + ")");
-            }
-
-                public boolean isAlive() {
-                            return this.health > 0;
+        public void takeDamage(int damage) {
+            this.health -= damage;
+                if (this.health < 0) {
+                                this.health = 0;
                 }
-
-                    // Abstract method: subclasses MUST define how they attack pls add nya huhu
-                        public abstract int attack();
-                }
-            
-            }
+                        System.out.println(name + " takes " + damage + " damage! (HP: " + health + "/" + maxHealth + ")");
         }
-    }
+
+        public boolean isAlive() {
+            return this.health > 0;
+        }
+
+        public int attack() {
+            System.out.println(name + " attacks for " + attackPower + " damage!");
+            return attackPower;
+        }
 }

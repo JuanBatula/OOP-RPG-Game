218833081f97690d38be5e45ff67a7e6fa17eb5b
Francis Benedict Gaviola <gaviolabenedict@gmail.com>
2026-04-21 03:29:57 +0000

    feat: Added initial methods in Character class

--- Character.java
+++ Character.java
@@ -12,6 +12,24 @@ public abstract class Character {
         this.attackPower = attackPower;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void takeDamage(int damage) {
+        this.health -= damage;
+        if (this.health < 0) {
+            this.health = 0;
+        }
+        System.out.println(name + " takes " + damage + " damage! (HP: " + health + "/" + maxHealth + ")");
+    }
+
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+
+    // Abstract method: subclasses MUST define how they attack pls add nya huhu
+    public abstract int attack();
 
 
 }

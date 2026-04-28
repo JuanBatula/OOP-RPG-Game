4bb9f4fe9f52fe8e983d1d107459e98f85413929
Vaughn Ouano <vaughneurielouano@example.com>
2026-04-28 11:27:45 +0800

    fix: add missing bracket in Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Enemy extends Character {
 
     private String difficulty;
@@ -23,30 +22,4 @@ public class Enemy extends Character {
         System.out.println(name + " attacks for " + attackPower + " damage!");
         return attackPower;
     }
-=======
-public class Enemy extends Character {
-
-    private String difficulty;
-    private int expValue;
-
-    public Enemy(String name, String difficulty) {
-        super(name, 80, 80, 8);
-        this.difficulty = difficulty;
-        this.expValue = 50;
-    }
-
-    public String getDifficulty() {
-        return difficulty;
-    }
-
-    public int getExpValue() {
-        return expValue;
-    }
-
-    @Override
-    public int attack() {
-        System.out.println(name + " attacks for " + attackPower + " damage!");
-        return attackPower;
-    }
->>>>>>> 2c58e37fc531d424051e15912577b405c8270680
 }

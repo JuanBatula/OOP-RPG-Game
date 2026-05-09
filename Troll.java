f43bd961e7ef1cc7eea801ba8b5ae5bb07a35d61
Angela Vem Limos <angelavem.limos@cit.edu>
2026-05-09 10:09:33 +0800

    feat(enemy): add Troll enemy with heavy damage and miss chance mechanics

---
+++ Troll.java
@@ -0,0 +1,23 @@
+public class Troll extends Enemy {
+
+  private static final double DAMAGE_MULTIPLIER = 1.5;
+  private static final double MISS_CHANCE = 0.20;
+
+  public Troll(int health, int attackPower, int defense) {
+    super("Troll", health, health, attackPower, defense, 150);
+  }
+
+  @Override
+  public int getXPReward() {
+    return xpReward;
+  }
+
+  @Override
+  public int getAttackDamage(int currentHp, int maxHp) {
+    if(Math.random() < MISS_CHANCE) {
+      System.out.println(name + "'s heavy club swing misses!");
+      return 0;
+    }
+    return (int)(this.attackPower * DAMAGE_MULTIPLIER);
+  }
+}

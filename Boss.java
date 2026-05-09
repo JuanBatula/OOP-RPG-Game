9d7794e9d2d58887716f77ca4c04404ba849000c
Angela Vem Limos <angelavem.limos@cit.edu>
2026-05-09 10:14:54 +0800

    feat(enemy): add Boss enemy with enrage mechanic at low health

---
+++ Boss.java
@@ -0,0 +1,23 @@
+public class Boss extends Enemy {
+  private static final double RAGE_THRESHOLD = 0.30;
+  private static final double RAGE_MULTIPLIER = 1.20;
+
+  public Boss(int health, int attackPower, int defense) {
+    super("Boss", health, health, attackPower, defense, 500);
+  }
+
+  @Override
+  public int getXPReward() {
+    return xpReward;
+  }
+
+  @Override
+  public int getAttackDamage(int currentHp, int maxHp) {
+    boolean enraged = (double) currentHp / maxHp < RAGE_THRESHOLD;
+    if(enraged) {
+      System.out.println(name + " is ENRAGED!");
+      return (int)(this.attackPower * RAGE_MULTIPLIER);
+    }
+    return this.attackPower;
+  }
+}
